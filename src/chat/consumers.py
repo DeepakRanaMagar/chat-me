1c573bae0168a315c4c232b1a1af960ff836df41
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-09-08 13:07:08 +0545

    chat init

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -4,10 +4,34 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self, ):
+        
         self.accept()
         self.send(text_data=json.dumps(
             {
                 'type': 'connection_established',
                 'message': 'You are now connected.'
+            }
+        ))
+
+
+    def receive(self, text_data):
+        
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        self.send(text_data=json.dumps(
+            {
+                'type': 'chat',
+                'message': message
+            }
+        ))
+
+    def chat_message(self, event):
+
+        message = event['message']
+
+        self.send(text_data = json.dumps(
+            {
+                'type': 'chat',
+                'message': message
             }
         ))
